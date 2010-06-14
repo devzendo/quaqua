9d005b39e527efcf37d6eb12f4852cc0639055c6
Matt Gumbley <matt.gumbley@devzendo.org>
2010-06-14 22:41:48 +0100

    Initial Commit

---
+++ install-quaqua.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+VER=5.2.1
+DIST=quaqua-${VER}.zip
+DIR=Quaqua/dist
+#unzip ${DIST} ${DIR}/libquaqua.jnilib ${DIR}/libquaqua64.jnilib ${DIR}/quaqua.jar
+#cd ${DIR}
+#zip ../../libquaqua-${VER}.zip libquaqua*
+#cp quaqua.jar ../../quaqua-5.2.1.jar 
+#cd ../..
+mvn install:install-file -Dfile=libquaqua-${VER}.zip \
+	-DgroupId=ch.randelshofer -DartifactId=libquaqua \
+	-Dversion=${VER} -Dpackaging=zip -DcreateChecksum=true \
+	-DgeneratePom=true
+mvn install:install-file -Dfile=quaqua-${VER}.jar \
+	-DgroupId=ch.randelshofer -DartifactId=quaqua \
+	-Dversion=${VER} -Dpackaging=jar -DcreateChecksum=true \
+	-DgeneratePom=true
+#rm -rf ${DIST}
