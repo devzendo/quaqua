baa69252e5ffd7d580f524cc220dda48a18fd31f
Matt Gumbley <matt.gumbley@devzendo.org>
2010-06-15 05:18:02 +0100

    list the installable files, give a prompt

--- install-quaqua.sh
+++ install-quaqua.sh
@@ -6,8 +6,11 @@ DISTDIR=${UNZIPDIR}/dist
 unzip ${DIST} ${DISTDIR}/libquaqua.jnilib ${DISTDIR}/libquaqua64.jnilib ${DISTDIR}/quaqua.jar
 cd ${DISTDIR}
 zip ../../libquaqua-${VER}.zip libquaqua*
-cp quaqua.jar ../../quaqua-5.2.1.jar 
+cp quaqua.jar ../../quaqua-${VER}.jar 
 cd ../..
+echo press return to install:
+ls -l quaqua-${VER}.jar libquaqua-${VER}.zip
+read TRASH
 mvn install:install-file -Dfile=libquaqua-${VER}.zip \
 	-DgroupId=ch.randelshofer -DartifactId=libquaqua \
 	-Dversion=${VER} -Dpackaging=zip -DcreateChecksum=true \
