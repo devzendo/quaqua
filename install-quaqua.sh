02aad255bd516fdad42fcac0a93b8c2eb119d611
Matt Gumbley <matt.gumbley@devzendo.org>
2010-06-15 05:10:07 +0100

    Do the unzip and remove after installing

--- install-quaqua.sh
+++ install-quaqua.sh
@@ -2,11 +2,11 @@
 VER=5.2.1
 DIST=quaqua-${VER}.zip
 DIR=Quaqua/dist
-#unzip ${DIST} ${DIR}/libquaqua.jnilib ${DIR}/libquaqua64.jnilib ${DIR}/quaqua.jar
-#cd ${DIR}
-#zip ../../libquaqua-${VER}.zip libquaqua*
-#cp quaqua.jar ../../quaqua-5.2.1.jar 
-#cd ../..
+unzip ${DIST} ${DIR}/libquaqua.jnilib ${DIR}/libquaqua64.jnilib ${DIR}/quaqua.jar
+cd ${DIR}
+zip ../../libquaqua-${VER}.zip libquaqua*
+cp quaqua.jar ../../quaqua-5.2.1.jar 
+cd ../..
 mvn install:install-file -Dfile=libquaqua-${VER}.zip \
 	-DgroupId=ch.randelshofer -DartifactId=libquaqua \
 	-Dversion=${VER} -Dpackaging=zip -DcreateChecksum=true \
@@ -15,4 +15,4 @@ mvn install:install-file -Dfile=quaqua-${VER}.jar \
 	-DgroupId=ch.randelshofer -DartifactId=quaqua \
 	-Dversion=${VER} -Dpackaging=jar -DcreateChecksum=true \
 	-DgeneratePom=true
-#rm -rf ${DIST}
+rm -rf ${DIR}
